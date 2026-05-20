f23a71eeca2cfc88941fa4b395c5b977bbfa8034
CreMarNic <cremarnic@gmail.com>
2026-05-20 13:55:51 +0200

    2 more copies

--- src/main/java/com/fittimer/controller/WebController.java
+++ src/main/java/com/fittimer/controller/WebController.java
@@ -1,6 +1,4 @@
-package com.fittimer.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+package com.fittimer.controllstereotype.Controller;
+import org.springframework.w    eb.bind.annotation.GetMapping;
 
 
